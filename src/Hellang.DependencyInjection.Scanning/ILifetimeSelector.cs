4d71019b0d728632389157f9fab0be75807da39d
Kristian Hellang <kristian@hellang.com>
2015-11-14 03:53:25 +0100

    Added XML docs to ILifetimeSelector

--- src/Hellang.DependencyInjection.Scanning/ILifetimeSelector.cs
+++ src/Hellang.DependencyInjection.Scanning/ILifetimeSelector.cs
@@ -4,12 +4,24 @@ namespace Microsoft.Extensions.DependencyInjection.Scanning
 {
     public interface ILifetimeSelector : IFluentInterface
     {
+        /// <summary>
+        /// Registers each matching concrete type with <see cref="ServiceLifetime.Singleton"/> lifetime.
+        /// </summary>
         void WithSingletonLifetime();
 
+        /// <summary>
+        /// Registers each matching concrete type with <see cref="ServiceLifetime.Scoped"/> lifetime.
+        /// </summary>
         void WithScopedLifetime();
 
+        /// <summary>
+        /// Registers each matching concrete type with <see cref="ServiceLifetime.Transient"/> lifetime.
+        /// </summary>
         void WithTransientLifetime();
 
+        /// <summary>
+        /// Registers each matching concrete type with the specified <paramref name="lifetime"/>.
+        /// </summary>
         void WithLifetime(ServiceLifetime lifetime);
     }
 }

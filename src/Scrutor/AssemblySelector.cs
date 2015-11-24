fbc47bbb362fd2512e80a2ce0635bb5cf62ab117
Christian Neuß <cneuss@1WorldSync.com>
2015-11-24 11:14:29 +0100

    - Upgrade from Beta 8 -> RC 1

--- src/Scrutor/AssemblySelector.cs
+++ src/Scrutor/AssemblySelector.cs
@@ -2,79 +2,79 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Microsoft.Framework.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Scrutor
 {
-    internal class AssemblySelector : IAssemblySelector, ISelector
-    {
-        public AssemblySelector()
-        {
-            Selectors = new List<ISelector>();
-        }
+	internal class AssemblySelector : IAssemblySelector, ISelector
+	{
+		public AssemblySelector()
+		{
+			Selectors = new List<ISelector>();
+		}
 
-        private List<ISelector> Selectors { get; }
+		private List<ISelector> Selectors { get; }
 
-        public IImplementationTypeSelector FromAssemblyOf<T>()
-        {
-            return FromAssembliesOf(typeof(T));
-        }
+		public IImplementationTypeSelector FromAssemblyOf<T>()
+		{
+			return FromAssembliesOf(typeof(T));
+		}
 
-        public IImplementationTypeSelector FromAssembliesOf(params Type[] types)
-        {
-            if (types == null)
-            {
-                throw new ArgumentNullException(nameof(types));
-            }
+		public IImplementationTypeSelector FromAssembliesOf(params Type[] types)
+		{
+			if (types == null)
+			{
+				throw new ArgumentNullException(nameof(types));
+			}
 
-            return FromAssembliesOf(types.AsEnumerable());
-        }
+			return FromAssembliesOf(types.AsEnumerable());
+		}
 
-        public IImplementationTypeSelector FromAssembliesOf(IEnumerable<Type> types)
-        {
-            if (types == null)
-            {
-                throw new ArgumentNullException(nameof(types));
-            }
+		public IImplementationTypeSelector FromAssembliesOf(IEnumerable<Type> types)
+		{
+			if (types == null)
+			{
+				throw new ArgumentNullException(nameof(types));
+			}
 
-            return FromAssemblies(types.Select(t => t.GetTypeInfo().Assembly));
-        }
+			return FromAssemblies(types.Select(t => t.GetTypeInfo().Assembly));
+		}
 
-        public IImplementationTypeSelector FromAssemblies(params Assembly[] assemblies)
-        {
-            if (assemblies == null)
-            {
-                throw new ArgumentNullException(nameof(assemblies));
-            }
+		public IImplementationTypeSelector FromAssemblies(params Assembly[] assemblies)
+		{
+			if (assemblies == null)
+			{
+				throw new ArgumentNullException(nameof(assemblies));
+			}
 
-            return FromAssemblies(assemblies.AsEnumerable());
-        }
+			return FromAssemblies(assemblies.AsEnumerable());
+		}
 
-        public IImplementationTypeSelector FromAssemblies(IEnumerable<Assembly> assemblies)
-        {
-            if (assemblies == null)
-            {
-                throw new ArgumentNullException(nameof(assemblies));
-            }
+		public IImplementationTypeSelector FromAssemblies(IEnumerable<Assembly> assemblies)
+		{
+			if (assemblies == null)
+			{
+				throw new ArgumentNullException(nameof(assemblies));
+			}
 
-            return AddSelector(assemblies.SelectMany(asm => asm.ExportedTypes));
-        }
+			return AddSelector(assemblies.SelectMany(asm => asm.ExportedTypes));
+		}
 
-        public void Populate(IServiceCollection services)
-        {
-            foreach (var selector in Selectors)
-            {
-                selector.Populate(services);
-            }
-        }
+		public void Populate(IServiceCollection services)
+		{
+			foreach (var selector in Selectors)
+			{
+				selector.Populate(services);
+			}
+		}
 
-        private IImplementationTypeSelector AddSelector(IEnumerable<Type> types)
-        {
-            var selector = new ImplementationTypeSelector(this, types);
+		private IImplementationTypeSelector AddSelector(IEnumerable<Type> types)
+		{
+			var selector = new ImplementationTypeSelector(this, types);
 
-            Selectors.Add(selector);
+			Selectors.Add(selector);
 
-            return selector;
-        }
-    }
+			return selector;
+		}
+	}
 }
--- src/Scrutor/ServiceDescriptorAttribute.cs
+++ src/Scrutor/ServiceDescriptorAttribute.cs
@@ -1,23 +1,24 @@
 ﻿using System;
-using Microsoft.Framework.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
 
 namespace Scrutor
 {
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
-    public class ServiceDescriptorAttribute : Attribute
-    {
-        public ServiceDescriptorAttribute() : this(null) { }
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+	public class ServiceDescriptorAttribute : Attribute
+	{
+		public ServiceDescriptorAttribute() : this(null) { }
 
-        public ServiceDescriptorAttribute(Type serviceType) : this(serviceType, ServiceLifetime.Transient) { }
+		public ServiceDescriptorAttribute(Type serviceType) : this(serviceType, ServiceLifetime.Transient) { }
 
-        public ServiceDescriptorAttribute(Type serviceType, ServiceLifetime lifetime)
-        {
-            ServiceType = serviceType;
-            Lifetime = lifetime;
-        }
+		public ServiceDescriptorAttribute(Type serviceType, ServiceLifetime lifetime)
+		{
+			ServiceType = serviceType;
+			Lifetime = lifetime;
+		}
 
-        public Type ServiceType { get; }
+		public Type ServiceType { get; }
 
-        public ServiceLifetime Lifetime { get; }
-    }
+		public ServiceLifetime Lifetime { get; }
+	}
 }

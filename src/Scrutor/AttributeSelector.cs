9396316a2a9c1af89de3689a3e6c82bfd9b9fe76
Martyn Messerli <Martyn.Messerli@autodesk.com>
2016-04-22 09:29:10 +0200

    Throw an exception when an ServiceDescriptor is applied with an invalid type or applied multiple time the same type.

--- src/Scrutor/AttributeSelector.cs
+++ src/Scrutor/AttributeSelector.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -20,9 +21,17 @@ namespace Scrutor
             {
                 var typeInfo = type.GetTypeInfo();
 
-                foreach (var attribute in typeInfo.GetCustomAttributes<ServiceDescriptorAttribute>())
+                var attributes = typeInfo.GetCustomAttributes<ServiceDescriptorAttribute>().ToArray();
+                // check whether they have the same service type
+                var duplicates = attributes.GroupBy(s => s).SelectMany(grp => grp.Skip(1));
+                if (duplicates.Any())
                 {
-                    var serviceType = attribute.ServiceType ?? type;
+                    throw new InvalidOperationException($"Type \"{type.FullName}\" has multiple ServiceDescriptors specified with the same service type.");
+                }
+
+                foreach (var attribute in attributes)
+                {
+                    var serviceType = GetServiceType(type, attribute);
 
                     var descriptor = new ServiceDescriptor(serviceType, type, attribute.Lifetime);
 
@@ -30,5 +39,23 @@ namespace Scrutor
                 }
             }
         }
+
+        private Type GetServiceType(Type type, ServiceDescriptorAttribute attribute)
+        {
+            var serviceType = attribute.ServiceType;
+            if (ReferenceEquals(null, serviceType))
+            {
+                return type;
+            }
+
+            if (!serviceType.IsAssignableFrom(type))
+            {
+                throw new InvalidOperationException($"Type \"{type.FullName}\" does not inherit or implement \"${serviceType}\".");
+            }
+
+            return serviceType;
+
+        }
+
     }
 }

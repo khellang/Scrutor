054aa67bb2a7cf2a6d1438497a1d79b793465a94
Adam Hathcock <adam@hathcock.uk>
2017-04-06 12:57:35 +0100

    Remove unused using statement

--- src/Scrutor/AttributeSelector.cs
+++ src/Scrutor/AttributeSelector.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Scrutor
 {

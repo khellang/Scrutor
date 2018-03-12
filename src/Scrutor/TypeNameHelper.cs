ba538330eb35b672f9cb6f11610937200812d057
Kristian Hellang <kristian@hellang.com>
2018-03-12 23:22:37 +0100

    Added full license and link to repo for TypeNameHelper

--- src/Scrutor/TypeNameHelper.cs
+++ src/Scrutor/TypeNameHelper.cs
@@ -1,5 +1,19 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+﻿// Copyright(c) .NET Foundation and Contributors
+//
+// All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+//
+// From https://github.com/aspnet/Common/blob/62018e351c9bed107075d0139b551fb086f9693d/shared/Microsoft.Extensions.TypeNameHelper.Sources/TypeNameHelper.cs
 
 using System;
 using System.Text;

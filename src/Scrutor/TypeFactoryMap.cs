baca8641ccc55604f6733c7ae9eb989796d951c8
Andrew Lock <andrewlock.net@gmail.com>
2018-08-28 22:48:39 +0100

    Add support for registering all interaces against a single implementation

---
+++ src/Scrutor/TypeFactoryMap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Scrutor
+{
+    internal struct TypeFactoryMap
+    {
+        public TypeFactoryMap(Func<IServiceProvider, object> implementationFactory, IEnumerable<Type> serviceTypes)
+        {
+            ImplementationFactory = implementationFactory;
+            ServiceTypes = serviceTypes;
+        }
+
+        public Func<IServiceProvider, object> ImplementationFactory { get; }
+
+        public IEnumerable<Type> ServiceTypes { get; }
+    }
+}

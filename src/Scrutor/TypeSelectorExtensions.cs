f60f908032f8cecc6500a13a27249a3d364f93b8
Kristian Hellang <kristian@hellang.com>
2023-09-01 09:12:36 +0200

    Remove Obsolete methods

--- src/Scrutor/TypeSelectorExtensions.cs
+++ src/Scrutor/TypeSelectorExtensions.cs
@@ -1,4 +1,3 @@
-using System;
 using JetBrains.Annotations;
 
 namespace Scrutor;
@@ -6,9 +5,6 @@ namespace Scrutor;
 [PublicAPI]
 public static class TypeSelectorExtensions
 {
-    [Obsolete("This method has been marked obsolete and will be removed in the next major version. Use " + nameof(FromTypes) + " instead.")]
-    public static IServiceTypeSelector AddType<T>(this ITypeSelector selector) => selector.FromType<T>();
-
     public static IServiceTypeSelector FromType<T>(this ITypeSelector selector)
     {
         Preconditions.NotNull(selector, nameof(selector));
@@ -16,18 +12,12 @@ public static class TypeSelectorExtensions
         return selector.FromTypes(typeof(T));
     }
 
-    [Obsolete("This method has been marked obsolete and will be removed in the next major version. Use " + nameof(FromTypes) + " instead.")]
-    public static IServiceTypeSelector AddTypes<T1, T2>(this ITypeSelector selector) => selector.FromTypes<T1, T2>();
-
     public static IServiceTypeSelector FromTypes<T1, T2>(this ITypeSelector selector)
     {
         Preconditions.NotNull(selector, nameof(selector));
 
         return selector.FromTypes(typeof(T1), typeof(T2));
     }
-
-    [Obsolete("This method has been marked obsolete and will be removed in the next major version. Use " + nameof(FromTypes) + " instead.")]
-    public static IServiceTypeSelector AddTypes<T1, T2, T3>(this ITypeSelector selector) => selector.FromTypes<T1, T2, T3>();
 
     public static IServiceTypeSelector FromTypes<T1, T2, T3>(this ITypeSelector selector)
     {

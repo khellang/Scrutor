e300f49326e01936e230cbbbdaa73232b0f95176
Kristian Hellang <kristian@hellang.com>
2023-09-01 09:52:25 +0200

    Extract methods

--- src/Scrutor/ServiceDescriptorExtensions.cs
+++ src/Scrutor/ServiceDescriptorExtensions.cs
@@ -8,28 +8,31 @@ internal static class ServiceDescriptorExtensions
     public static ServiceDescriptor WithImplementationFactory(this ServiceDescriptor descriptor, Func<IServiceProvider, object?, object> implementationFactory) =>
         new(descriptor.ServiceType, descriptor.ServiceKey, implementationFactory, descriptor.Lifetime);
 
-    public static ServiceDescriptor WithServiceKey(this ServiceDescriptor descriptor, string serviceKey)
+    public static ServiceDescriptor WithServiceKey(this ServiceDescriptor descriptor, string serviceKey) =>
+        descriptor.IsKeyedService ? ReplaceServiceKey(descriptor, serviceKey) : AddServiceKey(descriptor, serviceKey);
+
+    private static ServiceDescriptor ReplaceServiceKey(ServiceDescriptor descriptor, string serviceKey)
     {
-        if (descriptor.IsKeyedService)
+        if (descriptor.KeyedImplementationType is not null)
         {
-            if (descriptor.KeyedImplementationType is not null)
-            {
-                return new ServiceDescriptor(descriptor.ServiceType, serviceKey, descriptor.KeyedImplementationType, descriptor.Lifetime);
-            }
-
-            if (descriptor.KeyedImplementationInstance is not null)
-            {
-                return new ServiceDescriptor(descriptor.ServiceType, serviceKey, descriptor.KeyedImplementationInstance);
-            }
-
-            if (descriptor.KeyedImplementationFactory is not null)
-            {
-                return new ServiceDescriptor(descriptor.ServiceType, serviceKey, descriptor.KeyedImplementationFactory, descriptor.Lifetime);
-            }
-
-            throw new InvalidOperationException($"One of the following properties must be set: {nameof(ServiceDescriptor.KeyedImplementationType)}, {nameof(ServiceDescriptor.KeyedImplementationInstance)} or {nameof(ServiceDescriptor.KeyedImplementationFactory)}");
+            return new ServiceDescriptor(descriptor.ServiceType, serviceKey, descriptor.KeyedImplementationType, descriptor.Lifetime);
         }
 
+        if (descriptor.KeyedImplementationInstance is not null)
+        {
+            return new ServiceDescriptor(descriptor.ServiceType, serviceKey, descriptor.KeyedImplementationInstance);
+        }
+
+        if (descriptor.KeyedImplementationFactory is not null)
+        {
+            return new ServiceDescriptor(descriptor.ServiceType, serviceKey, descriptor.KeyedImplementationFactory, descriptor.Lifetime);
+        }
+
+        throw new InvalidOperationException($"One of the following properties must be set: {nameof(ServiceDescriptor.KeyedImplementationType)}, {nameof(ServiceDescriptor.KeyedImplementationInstance)} or {nameof(ServiceDescriptor.KeyedImplementationFactory)}");
+    }
+
+    private static ServiceDescriptor AddServiceKey(ServiceDescriptor descriptor, string serviceKey)
+    {
         if (descriptor.ImplementationType is not null)
         {
             return new ServiceDescriptor(descriptor.ServiceType, serviceKey, descriptor.ImplementationType, descriptor.Lifetime);
